d3e5cfa13b5b5264148ef89b590f2d21cfcefc1f
MainMake <405771487@qq.com>
2017-03-29 18:27:32 +0800

    commit message

---
+++ javascripts/json.js
@@ -0,0 +1,113 @@
+/**
+ * 2017/2/14.
+ */
+var Json = {
+	    project:[
+        {
+            name:'官网活动单页',
+            date:'2016.6',
+            detail:'css3翻转效果，swiper滑动banner,JS无缝滚动，选项卡',
+            image:['images/jsonImages/1.png','images/jsonImages/2.png','images/jsonImages/3.png','images/jsonImages/3.png'],
+            link:'http://t.cn/RJeN4cl'
+        },
+       {
+            name:'奇偶横竖排判断',
+            date:'2016.6',
+            detail:'奇偶横竖排判断',
+            image:['images/jsonImages/leo1.png','images/jsonImages/leo2.png','images/jsonImages/leo3.png','images/jsonImages/leo3.png'],
+            link:'demo/leo_if.html' 
+       },
+          {
+            name:'交叉选项卡',
+            date:'2016.6',
+            detail:'交叉选项卡',
+            image:['images/jsonImages/leo_tab1.png','images/jsonImages/leo_tab2.png','images/jsonImages/leo_tab3.png','images/jsonImages/leo_tab3.png'],
+            link:'demo/leo_tab.html' 
+       },
+          {
+            name:'图片切换',
+            date:'2016.6',
+            detail:'缩略图图片切换',
+            image:['images/jsonImages/leo_img1.png','images/jsonImages/leo_img2.png','images/jsonImages/leo_img3.png','images/jsonImages/leo_img3.png'],
+            link:'demo/leoImgTab.html' 
+       },
+         {
+            name:'价格统计',
+            date:'2016.9',
+            detail:'价格统计',
+            image:['images/jsonImages/code1.png','images/jsonImages/code2.png','images/jsonImages/code3.png','images/jsonImages/code3.png'],
+            link:'demo/code.html' 
+       },
+        {
+            name:'官网单页',
+            date:'2016.9',
+            detail:'css3翻转效果，swiper滑动banner,JS无缝滚动，选项卡',
+            image:['images/jsonImages/viacloud1.png','images/jsonImages/viacloud2.png','images/jsonImages/viacloud3.png','images/jsonImages/viacloud3.png'],
+            link:'http://t.cn/RJsw8NJ' 
+       },
+         {
+            name:'文本框修改',
+            date:'2017.6',
+            detail:'修改文本值',
+            image:['images/jsonImages/text1.png','images/jsonImages/text2.png','images/jsonImages/text3.png','images/jsonImages/text3.png'],
+            link:'demo/text.html' 
+       },
+         
+          {
+            name:'纪录片菜单栏',
+            date:'2017.3',
+            detail:'tab切换， 移入移出 show  hide',
+            image:['images/jsonImages/nav-1.png','images/jsonImages/nav-2.png','images/jsonImages/nav-3.png','images/jsonImages/nav-4.png'],
+            link:'demo/nav-tab.html' 
+       },
+        {
+            name:'tab-套 tab选项卡',
+            date:'2017.3',
+            detail:'附加索引值,num,实现tab切换',
+            image:['images/jsonImages/tab-info1.png','images/jsonImages/tab-info2.png','images/jsonImages/tab-info1.png','images/jsonImages/tab-info2.png'],
+            link:'demo/tab-info.html' 
+       },
+        {
+            name:'Rem(Rk)',
+            date:'2017.3',
+            detail:'rem抽奖页面',
+            image:['images/jsonImages/rk1.png','images/jsonImages/rk2.png','images/jsonImages/rk3.png','images/jsonImages/rk4.png'],
+            link:'demo/RK.html' 
+       },
+        {
+            name:'Rem(codeRandom)',
+            date:'2017.3',
+            detail:'rem排行抽奖',
+            image:['images/jsonImages/codeR1.png','images/jsonImages/codeR2.png','images/jsonImages/codeR1.png','images/jsonImages/codeR2.png'],
+            link:'demo/coderandom.html' 
+       }
+        
+    ],
+    work: [
+        {
+            company:'上海世纪互联',
+            job:'前端制作',
+            date:'2015年6月-2017年2月',
+            detail:'负责pc端，手机端页面的制作。负责完成页面交互效果的制作。与后端配合完成数据交互。',
+            logo:'images/jsonImages/logo_nav.png'
+        }
+    ],
+    myInfo:{                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+        name:'make33',
+        slogan:'二年前端经验',
+        edu:'专科 岳阳学院',
+        time:'1年零8月',
+        sex:'1994年 男',
+        city:'上海',
+        phone:'13818948470',
+        mail:'405771487@qq.com',
+        work:'前端工程师，全职，上海，8K-10K',
+        work1:'前端工程师',
+        nature:'全职',
+        hopeCity:'上海',
+        money:'8K-10K',
+        head:'images/head.jpg',
+        ps:''
+    }
+
+};
